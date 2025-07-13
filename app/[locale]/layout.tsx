c57bd133e40b318c9266e315dfb7f67032e08462
Ilyosbek <ilyosbekibroximov23z@gmail.com>
2025-07-13 15:47:52 +0500

    icon changed

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -32,7 +32,7 @@ export default async function RootLayout({
   return (
     <html lang={locale}>
       <head> 
-        <link rel="icon" href="/logo.svg" />
+        <link rel="icon" href="/marketplace-logo.svg" />
       </head>
       <body
         className={`${workSans.variable} antialiased`}
