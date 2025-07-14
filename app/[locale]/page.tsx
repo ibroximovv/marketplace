03024fd81fa098d094a2e0d968ad39c17279f3c3
Ilyosbek <ilyosbekibroximov23z@gmail.com>
2025-07-15 00:21:48 +0500

    .....

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,8 @@
+import Category from "@/modules/Category";
 import Collections from "@/modules/Collection";
 import Creators from "@/modules/Creators";
 import Hero from "@/modules/Hero";
+import NFTMore from "@/modules/NFTMore";
 import { getRequest } from "@/service/getRequest";
 
 export default async function Home() {
@@ -8,11 +10,15 @@ export default async function Home() {
   const auctions = await getRequest("/auctions");
   const auctionsResults = await getRequest("/auction-results");
   const artists = await getRequest("/user?role=ARTIST");
+  const categories = await getRequest("/categories");
+  const nft = await getRequest("/nfts?limit=3");
   return (
     <>
       <Hero auctions={auctions} artists={artists} actionResults={auctionsResults} />
       <Collections collection={collections} />
       <Creators artists={artists} />
+      <Category categories={categories}/>
+      <NFTMore nft={nft}/>
     </>
   );
 }
--- components/BrowseCategoriesCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BrowseCategoriesCard = () => {
-  return (
-    <div>BrowseCategoriesCard</div>
-  )
-}
-
-export default BrowseCategoriesCard
